7cef586e44259c2505229bc0dbab88337d7757af
Quynh <qpham2594@gmail.com>
2024-01-10 09:35:34 -0500

    initial files

---
+++ src/pages/home.jsx
@@ -0,0 +1,9 @@
+import styles from '../styles/home.module.css'
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className={styles.placeholder}>I'm the home page!</h1>
+    </main>
+  )
+}
